d07c5d1d43de425afc79138ea60356433455a16d
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-01-15 13:54:20 +0900

    filter-proxy - feat: filter 기초

--- filter-proxy/src/main/java/initializer/MvcWebApplicationInitializer.java
+++ filter-proxy/src/main/java/initializer/MvcWebApplicationInitializer.java
@@ -1,14 +1,18 @@
 package initializer;
 
+import config.AppConfig;
 import config.WebConfig;
+import jakarta.servlet.Filter;
+
 import org.springframework.lang.NonNull;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class<?>[]{AppConfig.class};  //밑에 getServletFilters를 위해.. 
     }
 
     @Override
@@ -21,4 +25,10 @@ public class MvcWebApplicationInitializer extends AbstractAnnotationConfigDispat
     protected String[] getServletMappings() {
         return new String[] {"/"};
     }
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {new DelegatingFilterProxy("realFilter")};
+	}
+    
 }
